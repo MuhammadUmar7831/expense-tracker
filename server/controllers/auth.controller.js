78ee44fb6d4d3995c38298b0cf1f12ef75bfbe86
21f-9114 <eishu847@gmail.com>
2024-06-27 16:27:12 +0500

    auth.api.js updated

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,17 +1,20 @@
-import { errorHandler } from "../errors/error.js";
-import User from "../models/user.model.js";
-import { generateToken, hashPassword, validatePassword } from "../utils/auth.utils.js";
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { errorHandler } from '../errors/error.js';
+import { generateStrongPassword } from '../utils/auth.utils.js';
 
 export const signup = async (req, res, next) => {
   const { name, email, password, avatar } = req.body;
-  const hashedPassword = hashPassword(password);
+  const salt = parseInt(process.env.SALT);
+  const hashedPassword = bcryptjs.hashSync(password, salt);
   const newUser = new User({ name, email, password: hashedPassword, avatar });
   try {
     await newUser.save();
-    const token = generateToken(newUser._id);
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...user } = newUser._doc;
     res
-      .cookie("access_token", token, { httpOnly: true })
+      .cookie("access token", token, { httpOnly: true })
       .status(201)
       .send({ user, success: true });
   } catch (error) {
@@ -28,14 +31,14 @@ export const signin = async (req, res, next) => {
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, "User not found!"));
-    const validPassword = validatePassword(password, validUser.password);
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
-    const token = generateToken(validUser._id);
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...user } = validUser._doc;
     res
-      .cookie("access_token", token, { httpOnly: true })
-      .status(201)
-      .send(user);
+      .cookie("access token", token, { httpOnly: true })
+      .status(200)
+      .send({ user, success: true });
   } catch (error) {
     next(error);
   }
@@ -46,23 +49,26 @@ export const googleOAuth = async (req, res, next) => {
   const user = await User.findOne({ email });
   try {
     if (!user) {
-      const password = Math.random().toString(36).slice(-8); // Generate a random password
-      const hashedPassword = hashPassword(password);
+      // Signup with Google OAuth
+      const password = generateStrongPassword();
+      const salt = parseInt(process.env.SALT);
+      const hashedPassword = bcryptjs.hashSync(password, salt);
       const newUser = new User({ name, email, password: hashedPassword, avatar });
       await newUser.save();
-      const token = generateToken(newUser._id);
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
       const { password: pass, ...userWithoutPassword } = newUser._doc;
       res
-        .cookie("access_token", token, { httpOnly: true })
+        .cookie("access token", token, { httpOnly: true })
         .status(201)
-        .send(userWithoutPassword);
+        .send({ user: userWithoutPassword, success: true });
     } else {
-      const token = generateToken(user._id);
+      // Signin with Google OAuth
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
       const { password: pass, ...userWithoutPassword } = user._doc;
       res
-        .cookie("access_token", token, { httpOnly: true })
-        .status(201)
-        .send(userWithoutPassword);
+        .cookie("access token", token, { httpOnly: true })
+        .status(200)
+        .send({ user: userWithoutPassword, success: true });
     }
   } catch (error) {
     next(error);
@@ -71,7 +77,8 @@ export const googleOAuth = async (req, res, next) => {
 
 export const signout = async (req, res, next) => {
   try {
-    res.clearCookie('access_token');
+    res.cookie('access token', null, { expires: new Date(0) });
+    res.clearCookie('access token');
     res.status(200).send({ success: true, message: 'Signed out successfully' });
   } catch (error) {
     next(error);
