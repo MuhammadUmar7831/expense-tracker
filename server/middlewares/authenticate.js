b34a8d954747030bb109263cc23378fedd185eed
21f-9114 <eishu847@gmail.com>
2024-06-27 15:23:39 +0500

    server side

--- server/middlewares/authenticate.js
+++ server/middlewares/authenticate.js
@@ -2,13 +2,13 @@ import { errorHandler } from "../errors/error.js";
 import jwt from "jsonwebtoken";
 
 export const authenticate = async (req, res, next) => {
-  const token = req.cookies["access token"];
-  if (!token) return next(errorHandler(401, "please login to continue."));
+  const token = req.cookies["access_token"];
+  if (!token) return next(errorHandler(401, "Please login to continue."));
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = decoded.id;
     next();
   } catch (error) {
-    next(error);
+    next(errorHandler(401, "Invalid token, please login again."));
   }
 };
