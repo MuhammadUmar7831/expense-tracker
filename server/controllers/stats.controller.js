971066a888a9c594806c89d377564ed720ce5f9e
Muhammad Umar <mu8494759@gmail.com>
2024-06-28 13:19:07 +0500

    barchart route

--- server/controllers/stats.controller.js
+++ server/controllers/stats.controller.js
@@ -33,3 +33,38 @@ export const overview = async (req, res, next) => {
     next(error);
   }
 };
+
+export const barchart = async (req, res, next) => {
+  try {
+    const userId = req.userId;
+
+    const budgets = await Budget.find({ user: userId })
+      .sort({ createdAt: -1 })
+      .limit(5);
+
+    const budgetData = [];
+    for (const budget of budgets) {
+      const expenses = await Expense.aggregate([
+        { $match: { budget: budget._id } },
+        { $group: { _id: "$budget", totalSpending: { $sum: "$amount" } } },
+      ]);
+
+      const totalSpending = expenses.length > 0 ? expenses[0].totalSpending : 0;
+
+      budgetData.push({
+        budgetId: budget._id,
+        budgetName: budget.name,
+        budgetAmount: budget.amount,
+        totalSpending: totalSpending,
+      });
+    }
+
+    res.status(200).send({
+      success: true,
+      budgetData,
+      message: "barchart of expense sent",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- server/routes/stats.route.js
+++ server/routes/stats.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { overview } from "../controllers/stats.controller.js";
+import { barchart, overview } from "../controllers/stats.controller.js";
 import { authenticate } from "../middlewares/authenticate.js";
 
 const router = express.Router();
 
 router.get('/overview', authenticate, overview);
+router.get('/barchart', authenticate, barchart);
 // router.get('/recentBudgets/:userId', recentBudgets);
 // router.get('/recentExpenses/:userId', recentExpenses);
 
