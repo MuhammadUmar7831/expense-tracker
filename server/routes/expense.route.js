3bd4b5ffc918277396a479ad62ffc4c9e39a4cc8
Muhammad Umar <mu8494759@gmail.com>
2024-06-28 17:12:00 +0500

    authenticate middleware added

--- server/routes/expense.route.js
+++ server/routes/expense.route.js
@@ -1,11 +1,17 @@
 import express from "express";
 
-import { getExpense, addExpense, updateExpense, deleteExpense } from "../controllers/expense.controller.js";
+import {
+  getExpense,
+  addExpense,
+  updateExpense,
+  deleteExpense,
+} from "../controllers/expense.controller.js";
+import { authenticate } from "../middlewares/authenticate.js";
 const router = express.Router();
 
-router.get('/get/:expenseId', getExpense);
-router.post('/add', addExpense);
-router.put('/update/:expenseId', updateExpense);
-router.delete('/delete/:expenseId', deleteExpense);
+router.get("/get/:expenseId", authenticate, getExpense);
+router.post("/add", authenticate, addExpense);
+router.put("/update/:expenseId", authenticate, updateExpense);
+router.delete("/delete/:expenseId", authenticate, deleteExpense);
 
 export default router;
