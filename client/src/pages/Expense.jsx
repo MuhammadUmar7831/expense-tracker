c26a528722ff4648d77d586cf609188d2235f466
muntaha-hamid <muntahahamid67@gmail.com>
2024-06-28 22:28:26 +0500

    style file added

--- client/src/pages/Expense.jsx
+++ client/src/pages/Expense.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React from 'react';
+import "../styles/expense.css";
 
-export default function Expense() {
+const Expense = () => {
   return (
-    <div>Expense</div>
-  )
-}
+    <table  className="expense-table" >
+       <thead>
+      <tr >
+        <th>Name</th>
+        <th>Amount</th>
+        <th>Date</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr >
+        <td>Muntaha</td>
+        <td>500$</td>
+        <td>27/06/2024</td>
+
+      </tr>
+
+      <tr >
+        <td>Yaseen</td>
+        <td>100$</td>
+        <td>25/06/2004</td>
+
+      </tr>
+
+      <tr>
+        <td>Umar</td>
+        <td>500$</td>
+        <td>30/06/2004</td>
+
+      </tr>
+
+      <tr >
+        <td>Mubasher</td>
+        <td>50$</td>
+        <td>27/07/2004</td>
+
+      </tr>
+
+      <tr>
+        <td>Ali</td>
+        <td>500$</td>
+        <td>20/06/2004</td>
+
+      </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default Expense;
