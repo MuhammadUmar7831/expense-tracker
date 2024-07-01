283168dd94b456010bc7a22e71248c42dd539136
Muhammad Umar <mu8494759@gmail.com>
2024-07-01 14:32:46 +0500

    Budgets routes done

--- server/models/Budget.Model.js
+++ server/models/Budget.Model.js
@@ -1,24 +1,28 @@
 import mongoose from "mongoose";
 
-const budgetSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const budgetSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      min: [1, "Amount must be atleast $1"],
+    },
+    emoji: {
+      type: String,
+      default: "💸",
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  emoji: {
-    type: String,
-    default: "💸",
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Budget = mongoose.model("Budget", budgetSchema);
 
