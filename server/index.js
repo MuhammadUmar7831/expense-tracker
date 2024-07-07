6089b4a3e87f9285d65214bbb70f6e34d2067015
Muhammad Umar <mu8494759@gmail.com>
2024-07-07 18:25:13 +0500

    all origin cors

--- server/index.js
+++ server/index.js
@@ -17,7 +17,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    // origin: process.env.CLIENT_URL,
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
