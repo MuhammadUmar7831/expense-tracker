fed500faaa7cb518dc9f66506a15d991798f51f0
Muhammad Umar <mu8494759@gmail.com>
2024-07-07 17:53:36 +0500

    comment css file

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -5,7 +5,7 @@ import LogoIcon from "../interface/Svgs/LogoIcon";
 import EyeOnIcon from "../interface/Svgs/EyeOnIcon";
 import EyeOffIcon from "../interface/Svgs/EyeOffIcon";
 import useSignIn from "../hooks/useSignIn";
-import "../styles/signin.css"
+// import "../styles/signin.css"
 
 export default function SignIn() {
   const {
